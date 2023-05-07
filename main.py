0f1b7d22da1f4f239756fcacf5376c5ccde89cdf
Mladen Žurkić <mladen.zurkic00@gmail.com>
2023-05-08 01:25:14 +0200

    Subdirectory calculation added
    - Added calculations with subdirectories
    - Print is separate for a subdirectory and total for directory

--- main.py
+++ main.py
@@ -1,28 +1,48 @@
 import datetime
 import os
 
-
 def with_moviepy(filename):
     from moviepy.editor import VideoFileClip
     clip = VideoFileClip(filename)
     duration       = clip.duration
+    clip.close()
     return int(duration)
+
+
+def getDurationForDirectory(path, level):
+    filenames = os.listdir(path)
+    first = True
+    subtotalSeconds = 0
+    totalSeconds = 0
+    for filename in filenames:
+        if(os.path.isdir((path + "\\" + filename))):
+            nextPath = (path + "\\" + filename)
+            totalSeconds += getDurationForDirectory(nextPath, level+1)
+
+
+        if(filename.lower().endswith(extensions)):
+            if(first):
+                index = path.rfind("\\")
+                print("\t" * (level - 1) + path[index + 1:])
+                first = False
+            print("\t" * (level-1) + "├──" +  filename[:40] + "...  - ", end="")
+            seconds = with_moviepy(path + '\\' + filename)
+            subtotalSeconds += seconds
+            totalSeconds += seconds
+            print(str(datetime.timedelta(seconds=seconds)), end="\n")
+    if(not first):
+        print("\t" * (level-1) + "Subtotal: " + str(datetime.timedelta(seconds=subtotalSeconds)))
+    return totalSeconds
+
 
 
 if __name__ == '__main__':
     extensions = ('.mp4', '.flv', '.webm', '.mkv', 'vob', '.avi', '.m4v')
+    allDirs = 0
     dirc = input("input directory: ")
+    # allDirs = input("Search all subfolders(1) or only this directory(0)? ")
     print("Searching for most popular video formats..")
-    filenames= os.listdir(dirc)
-    totalSeconds = 0
 
-    for filename in filenames:
-        if(filename.lower().endswith(extensions)):
-            print(filename)
-            seconds = with_moviepy(dirc + '\\' + filename)
-            totalSeconds += seconds
-            print(str(datetime.timedelta(seconds=seconds)))
-            print("\n")
-    print("Total duration: " + str(datetime.timedelta(seconds=totalSeconds)))
+    total = getDurationForDirectory(dirc, 1)
 
-    input("Press enter to exit")
+    print("TOTAL: " +str(datetime.timedelta(seconds=total)))
