2e35696efd2e2193010b5b299430c1ff0554a4e2
Mladen Žurkić <mladen.zurkic00@gmail.com>
2023-09-12 17:44:21 +0200

    Better print
    - Printing format needs a few small changes, but everything works good

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ def with_moviepy(filename):
     return int(duration)
 
 
+def checkAllSubfolders(path):
+    filenames = os.listdir(path)
+    for filename in filenames:
+        if(os.path.isdir((path + "\\" + filename))):
+            return checkAllSubfolders(path + "\\" + filename)
+        if(filename.lower().endswith(extensions)):
+            return True
+    return False
+
 def getDurationForDirectory(path, level):
     filenames = os.listdir(path)
     first = True
@@ -17,22 +26,30 @@ def getDurationForDirectory(path, level):
     for filename in filenames:
         if(os.path.isdir((path + "\\" + filename))):
             nextPath = (path + "\\" + filename)
+
+            #check all subfolders for videos
+            #if(not checkAllSubfolders(nextPath)):
+               # continue
+
+            print('\033[1m' + os.path.basename(path))
+            print( "\t" * (level - 1) + "└──", end="")
             totalSeconds += getDurationForDirectory(nextPath, level+1)
 
 
         if(filename.lower().endswith(extensions)):
             if(first):
                 index = path.rfind("\\")
-                print("\t" * (level - 1) + path[index + 1:])
+                print('\033[1m' + path[index + 1:])
                 first = False
-            stringTest = "\t" * (level-1) + "├──" +  filename[:40] + "...  - "
+            stringTest = '\033[0m' + "\t" * (level-1) + "├──" +  filename[:40] + "...  - "
             print(f'{stringTest:<50}', end="")
             seconds = with_moviepy(path + '\\' + filename)
             subtotalSeconds += seconds
             totalSeconds += seconds
             print(str(datetime.timedelta(seconds=seconds)), end="\n")
     if(not first):
-        print("\t" * (level-1) + "Subtotal: " + str(datetime.timedelta(seconds=subtotalSeconds)))
+        print('\033[1m' + "\t" * (level-1) + "Subtotal: " + str(datetime.timedelta(seconds=subtotalSeconds)))
+        print('\033[0m')
     return totalSeconds
 
 
@@ -47,9 +64,10 @@ if __name__ == '__main__':
             exit(0);
         # allDirs = input("Search all subfolders(1) or only this directory(0)? ")
 
-        print("Searching for most popular video formats..")
+        print("Searching for most popular video formats..\n")
         total = getDurationForDirectory(dirc, 1)
         allTotal += total;
-
-        print(f'{"TOTAL:":<10}' +str(datetime.timedelta(seconds=total)))
-        print(f'{"ALL TOTAL:":<10}' + str(datetime.timedelta(seconds=allTotal)))
+        print('\033[1m')
+        print(f'{"TOTAL:":<11}' +str(datetime.timedelta(seconds=total)))
+        print(f'{"ALL TOTAL: ":<11}' + str(datetime.timedelta(seconds=allTotal)))
+        print('\033[0m')
